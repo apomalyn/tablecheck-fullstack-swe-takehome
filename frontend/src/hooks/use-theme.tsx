de5c96056e3e4f077f415707a629381c1327a983
apomalyn <chretienxavier42@gmail.com>
2024-11-15 22:46:50 +0900

    refactor: Refactor the `use-theme` hook to follow eslint guidelines.

--- frontend/src/hooks/use-theme.tsx
+++ frontend/src/hooks/use-theme.tsx
@@ -1,34 +1,45 @@
-import { createContext, ReactNode, useCallback, useContext, useState } from "react";
+import {
+    createContext,
+    ReactNode,
+    useCallback,
+    useContext,
+    useState,
+} from "react";
 
 interface ThemeContextType {
     isDarkMode: boolean;
     toggleDarkMode: () => void;
 }
+
 interface IProps {
     children: ReactNode;
 }
+
 export const ThemeContext = createContext<ThemeContextType | null>(null);
 
-const ThemeContextProvider = ({ children }: IProps) => {
-    const [isDarkMode, setIsDarkMode] = useState(false);
+export default function ThemeContextProvider({ children }: IProps) {
+    const [isDarkMode, setIsDarkMode] = useState(true);
 
     const toggleDarkMode = useCallback(() => {
         setIsDarkMode((prev) => !prev);
     }, [isDarkMode]);
-    const value = {
-        isDarkMode,
-        toggleDarkMode,
-    };
-    return (
-        <ThemeContext.Provider value={value}>{children}</ThemeContext.Provider>
-    );
-};
-export default ThemeContextProvider;
 
-export const useTheme = () => {
+    return (
+        <ThemeContext.Provider
+            value={{
+                isDarkMode,
+                toggleDarkMode,
+            }}
+        >
+            {children}
+        </ThemeContext.Provider>
+    );
+}
+
+export function useTheme() {
     const context = useContext(ThemeContext);
     if (context === null) {
         throw new Error("Context must be used within a context provider");
     }
     return context;
-};
+}
