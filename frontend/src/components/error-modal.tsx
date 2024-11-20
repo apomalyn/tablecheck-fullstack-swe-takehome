4519cae31a7fb38d51f76e42fe53ee996aebb4fc
apomalyn <chretienxavier42@gmail.com>
2024-11-20 09:20:35 +0900

    feat: Implement ErrorModal to inform the user when something went wrong

---
+++ frontend/src/components/error-modal.tsx
@@ -0,0 +1,50 @@
+import {
+    Button,
+    Modal,
+    ModalBody,
+    ModalContent,
+    ModalFooter,
+    ModalHeader,
+} from "@nextui-org/react";
+import { useTranslation } from "react-i18next";
+import { useTheme } from "@hooks/index.tsx";
+
+interface IErrorModalProps {
+    isOpen: boolean;
+    onOpenChange: (isOpen: boolean) => void;
+}
+
+export default function ErrorModal({
+                                        isOpen,
+                                        onOpenChange,
+                                    }: IErrorModalProps) {
+    const { t } = useTranslation();
+    const { isDarkMode } = useTheme();
+
+    return (
+        <Modal
+            className={`${isDarkMode ? "dark" : "light"} text-foreground bg-content1`}
+            isOpen={isOpen}
+            onOpenChange={onOpenChange}
+            backdrop="blur"
+        >
+            <ModalContent>
+                {(onClose) => (
+                    <>
+                        <ModalHeader>
+                            <p className="text-xl">{t("error_title")}</p>
+                        </ModalHeader>
+                        <ModalBody>
+                            <p>{t("error_try_again")}</p>
+                        </ModalBody>
+                        <ModalFooter>
+                            <Button variant="light" onPress={onClose}>
+                                {t("ok")}
+                            </Button>
+                        </ModalFooter>
+                    </>
+                )}
+            </ModalContent>
+        </Modal>
+    );
+}
--- frontend/src/components/index.tsx
+++ frontend/src/components/index.tsx
@@ -1,4 +1,5 @@
 import AppBar from "./app-bar.tsx";
 import CancelModal from "./cancel-modal.tsx";
+import ErrorModal from "@components/error-modal.tsx";
 
-export { AppBar, CancelModal };
+export { AppBar, CancelModal, ErrorModal };
